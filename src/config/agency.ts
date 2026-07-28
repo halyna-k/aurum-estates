5fd7e23c96cb5dc2b4508dc746b9920bff478ab6
galyna-k <hkzlvs@gmail.com>
2026-07-28 23:08:11 +0100

    Add agency configuration file
    Centralizes agency-specific defaults for use across the app.

---
+++ src/config/agency.ts
@@ -0,0 +1,19 @@
+export const agencyConfig = {
+  name: "Aurum Estates",
+
+  email: "info@aurumestates.com",
+
+  location: "London",
+
+  market: "UK Residential Lettings",
+
+  currency: "GBP",
+
+  affordabilityMultiplier: 30,
+
+  minimumTenancyMonths: 12,
+
+  viewingDurationMinutes: 30,
+
+  allowGuarantor: true,
+};
